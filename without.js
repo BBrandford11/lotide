77c6f689b4a4f1b7370fd9c9d96f3a3980c2ba13
BBrandford11 <brandfordbryce@gmail.com>
2022-02-09 10:27:00 -0700

    fixed without problem

--- without.js
+++ without.js
@@ -1,16 +1,13 @@
-const without = function(source, itemsToRemove){
-  let filteredlist = []
-  for (let x = 0; x < source.length; x++){
-    if (source[x] !== itemsToRemove[x]){
-      filteredlist.push(source[x])
+const without = function(source, itemsToRemove) {
+  const removedItems = [];
+  for (let i = 0; i < source.length; i++) {
+    if(!itemsToRemove.includes(source[i])) {
+      removedItems.push(source[i])
     }
-
+    
   }
-  return filteredlist
-
-  
-}
-
+  return removedItems;
+};
 
 const eqArrays = function(first, second) {
   
