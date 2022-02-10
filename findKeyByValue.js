ad7f82a44714d0045809ab6a5049107b09a5f8d3
BBrandford11 <brandfordbryce@gmail.com>
2022-02-09 19:59:37 -0700

    find key value

---
+++ findKeyByValue.js
@@ -0,0 +1,31 @@
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("😊😊😊 Assertion Passed: [" + actual + "] === [" + expected + "]");
+  } else {
+    console.log("😭😭😭 Assertion Failed: [" + actual + "] !== [" + expected + "]");
+  }
+};
+
+const findKeyByValue = function(object, value) {
+  //objects.keys to search thru keys
+  //for of loop over keys
+  let keys = Object.keys(object)
+  for(let key of keys) {
+    if (object[key] === value) {
+      return key;
+    }
+    console.log(key)
+  }
+}
+// TEST CODE
+
+
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+//console.log(findKeyByValue(bestTVShowsByGenre, "The Wire"))
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
