bdc3ece50a588f2852d12895ec6997f8fff9a5d2
BBrandford11 <brandfordbryce@gmail.com>
2022-02-14 16:07:16 -0700

    head test with mocha

--- test/assertEqualTest.js
+++ test/assertEqualTest.js
@@ -1,8 +1,17 @@
-const assertEqual = require("../assertEqual")
-const head = require("../head.js")
+const assert = require("chai").assert;
+const head = require("../head.js");
 // TEST CODE
 // assertEqual("Lighthouse Labs", "Bootcamp");
 // assertEqual(1, 1);
 
-assertEqual(head([5, 6, 7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+// assertEqual(head([5, 6, 7]), 5);
+// assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+
+describe("#head", () => {
+  it("returns 1 for [1, 2, 3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+  it("returns 1 for [1, 2, 3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+});
