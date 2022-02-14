a6cab549ea652d89b6991d318a46f2956dce6683
BBrandford11 <brandfordbryce@gmail.com>
2022-02-14 16:18:52 -0700

    tail test with mocha

--- test/tailTests.js
+++ test/tailTests.js
@@ -1,5 +1,16 @@
 const tail = require("../tail")
-const assertEqual = require("../assertEqual")
-const words = ["Yo Yo", "Lighthouse", "Labs", "check"]
+const assert = require("chai").assert;
+//const words = ["Yo Yo", "Lighthouse", "Labs", "check"]
 
-assertEqual(tail(words), "check")
+//assertEqual(tail(words), "check")
+describe("Tail", () => {
+  it("returns 3 for [1, 2, 3]", () => {
+    assert.strictEqual(tail([1, 2, 3]), 3);
+  });
+  it("returns 14 for [1, 2, 3, 14]", () => {
+    assert.strictEqual(tail([1, 2, 3, 14]), 14);
+  });
+  it("returns check for Yo Yo, Lighthouse, Labs, check", () => {
+    assert.strictEqual(tail(["Yo Yo", "Lighthouse", "Labs", "check"]), "check");
+  });
+});
